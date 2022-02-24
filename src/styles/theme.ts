a5f6a08a8e3e015b400b16d923417036a9652e32
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-24 08:11:17 +0100

    update

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,28 +2,24 @@ import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
+  colors: {
+    main: {},
   },
 });
 declare module '@mui/material/styles' {
   interface Theme {
-    status: {
-      danger: string;
+    colors: {
+      primary: {
+        main: string;
+      };
     };
   }
   // allow configuration using `createTheme`
   interface ThemeOptions {
-    status?: {
-      danger?: string;
+    colors?: {
+      primary?: {
+        main?;
+      };
     };
   }
 }
