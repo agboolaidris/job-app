414de51abf43074c97e794c38d5ce4d199db5d00
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-24 14:29:39 +0100

    update

--- src/components/layout/toolbar.tsx
+++ src/components/layout/toolbar.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@mui/material';
+import Link from 'next/link';
+import { Box, Theme } from '@mui/material';
+import styled from '@emotion/styled';
+const AppBar = styled.nav<{ theme?: Theme }>`
+  height: 80px;
+  display: flex;
+  align-items: center;
+  padding: 0 min(5%, 30px);
+  .logo {
+    text-decoration: none;
+    font-size: 1.5rem;
+    font-weight: bolder;
+    color: ${({ theme }) => theme.colors.secondary.main};
+  }
+`;
 
 function Index() {
   return (
-    <AppBar position="sticky">
-      <Toolbar>Welcome</Toolbar>
+    <AppBar>
+      <Box>HHHHHH</Box>
+      <Box sx={{ flexGrow: 1 }} />
+
+      <Link href="/">
+        <a className="logo">_IRIS_</a>
+      </Link>
+
+      <Box sx={{ flexGrow: 1 }} />
+      <Box>HHHHHH</Box>
     </AppBar>
   );
 }
