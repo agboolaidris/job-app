291ef9d4b1af27d3ed3af5c5cedafdacc4cca200
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-25 06:59:06 +0100

    update

--- src/components/layout/navItem.tsx
+++ src/components/layout/navItem.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
+import MuiMenuItem from '@mui/material/MenuItem';
+import MUIMenu from '@mui/material/Menu';
+import { Divider, Avatar } from '@mui/material';
 import styled from '@emotion/styled';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Logout from '@mui/icons-material/Logout';
 
+const MenuItemStyled = styled.a``;
+const MenuItemDropDown = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: red;
+  position: absolute;
+  left: 0;
+`;
+
+interface MenuItemProps {
+  children: ReactNode;
+}
+export const MenuItem = ({ children }: MenuItemProps) => {
+  return (
+    <>
+      <MenuItemStyled>{children}</MenuItemStyled>
+    </>
+  );
+};
+
+// The Menu Wrapper
 const MenuWrapper = styled.div`
   display: flex;
+  cursor: pointer;
+  ${MenuItemStyled} {
+    margin-left: 20px;
+    font-size: 0.8rem;
+    opacity: 0.7;
+    &:first-child {
+      margin-left: 0;
+    }
+    &:hover {
+      font-weight: bolder;
+      opacity: 1;
+    }
+  }
 `;
 
 export function Menu() {
   return (
     <MenuWrapper>
-      <span>Men</span>
-      <span>Men</span>
-      <span>Men</span>
+      <MenuItem>Men</MenuItem>
+      <MenuItem>Women</MenuItem>
+      <MenuItem>Unisex</MenuItem>
+      <MenuItem>Brand</MenuItem>
     </MenuWrapper>
   );
 }
--- src/components/layout/toolbar.tsx
+++ src/components/layout/toolbar.tsx
@@ -7,6 +7,7 @@ const AppBar = styled.nav<{ theme?: Theme }>`
   height: 80px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 min(5%, 30px);
   .logo {
     text-decoration: none;
@@ -22,13 +23,11 @@ function Index() {
       <Box>
         <Menu />
       </Box>
-      <Box sx={{ flexGrow: 1 }} />
 
       <Link href="/">
         <a className="logo">_IRIS_</a>
       </Link>
 
-      <Box sx={{ flexGrow: 1 }} />
       <Box>HHHHHH</Box>
     </AppBar>
   );
