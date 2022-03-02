2a447da6980b1b1181b3ec88334857343b750e1d
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-02 14:24:10 +0100

    update

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,14 +3,23 @@ import { Container, Theme } from '@mui/material';
 import React from 'react';
 
 const Wrap = styled.div<{ theme?: Theme }>`
-  // background: ${({ theme }) => theme.colors.primary.dark};
+  background-color: ${({ theme }) => theme.colors.primary.dark};
   padding: 100px 0;
+  .flex {
+    display: flex;
+  }
 `;
 
 function Footer() {
   return (
     <Wrap>
-      <Container>HHHHHHH</Container>
+      <Container>
+        <div className="flex">
+          <div>
+            <p className="title"></p>
+          </div>
+        </div>
+      </Container>
     </Wrap>
   );
 }
