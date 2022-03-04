93e318febe5e37b1f270197cf7823eea00f4ba9c
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-04 08:14:19 +0100

    update

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+const Wrap = styled(Box)`
+  width: 100%;
+  border: 2px solid red;
+  min-height: 40px;
+`;
 
 function Button() {
-  return <div>B</div>;
+  return <Wrap></Wrap>;
 }
 
 export default Button;
