6d6dbd459e9e60c84bbf7340c0647413670ded82
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-06 10:01:26 +0100

    update

--- src/components/main/gridImage.tsx
+++ src/components/main/gridImage.tsx
@@ -1,27 +1,59 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import { ImageList, Typography, ImageListItem, Theme } from '@mui/material';
+import { Link } from '../shared/link';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+const ImgBox = styled.div<{ theme?: Theme }>`
+  width: 100%;
+  height: 100%;
 
-function srcset(image, size, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
+  a {
+    opacity: 1;
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: relative;
+    img {
+      transition: all 0.3s ease-out;
 
+      &:hover {
+        overflow: hidden;
+        transform: scale(1.1);
+      }
+    }
+    h3 {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      z-index: 20;
+      transform: translate(-50%);
+      color: ${({ theme }) => theme.colors.primary.dark};
+    }
+  }
+`;
 
 export default function QuiltedImageList() {
   return (
-    <ImageList variant="quilted" cols={4} rowHeight={300}>
+    <ImageList
+      variant="quilted"
+      sx={{ overflow: 'hidden', height: { xs: 400, md: 600 } }}
+      cols={4}
+      rowHeight="auto"
+    >
       {itemData.map((item) => (
         <ImageListItem
           key={item.img}
           cols={item.cols || 1}
           rows={item.rows || 1}
         >
-          <h1>HELLO WORLD</h1>
+          <ImgBox>
+            <Link href="/">
+              <Image src={item.img} layout="fill" alt={item.img} />
+              <Typography component="h3" sx={{ fontSize: { md: '2.5rem' } }}>
+                {item.title}
+              </Typography>
+            </Link>
+          </ImgBox>
         </ImageListItem>
       ))}
     </ImageList>
@@ -30,22 +62,22 @@ export default function QuiltedImageList() {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: '/jacket.jpg',
+    title: 'Men',
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
+    img: '/mansuit.jpg',
+    title: 'Wedding',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
+    img: '/womansuit.jpg',
+    title: 'Women',
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: '/suit.jpg',
+    title: 'Unisex',
     cols: 2,
   },
 ];
