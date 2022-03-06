bbc453ded1e6c1cebadb6177328714d591a3ea44
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-06 14:29:52 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { Container, Grid } from '@mui/material';
 import Welcome from '../components/landingPage/welcome';
 import GridImage from '../components/main/gridImage';
+import InfoCard from '../components/shared/infoCard';
 import Main from '../layout/main';
 
 const images = ['/jacket.jpg', 'suit.jpg', 'womansuit.jpg', 'mansuit.jpg'];
@@ -7,6 +9,13 @@ const images = ['/jacket.jpg', 'suit.jpg', 'womansuit.jpg', 'mansuit.jpg'];
 const Home = () => (
   <Main>
     <Welcome images={images} />
+    <Container>
+      <Grid container spacing={2}>
+        <Grid xs={12} sm={6} md={3}>
+          <InfoCard />
+        </Grid>
+      </Grid>
+    </Container>
     <GridImage />
   </Main>
 );
