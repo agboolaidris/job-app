c4050783af090dfd34c0869a6b580a4356c15b60
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-06 07:42:21 +0100

    update

--- src/components/main/gridImage.tsx
+++ src/components/main/gridImage.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
-function GridImage() {
-  return <div>GridImage</div>;
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
 }
 
-export default GridImage;
+
+export default function QuiltedImageList() {
+  return (
+    <ImageList variant="quilted" cols={4} rowHeight={300}>
+      {itemData.map((item) => (
+        <ImageListItem
+          key={item.img}
+          cols={item.cols || 1}
+          rows={item.rows || 1}
+        >
+          <h1>HELLO WORLD</h1>
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+    cols: 2,
+  },
+];
