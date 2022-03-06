9f1d5ebaa98633153a633123c7952aece597d1ff
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-06 14:43:12 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,18 @@ const images = ['/jacket.jpg', 'suit.jpg', 'womansuit.jpg', 'mansuit.jpg'];
 const Home = () => (
   <Main>
     <Welcome images={images} />
-    <Container>
-      <Grid container spacing={2}>
-        <Grid xs={12} sm={6} md={3}>
+    <Container sx={{ paddingY: 4 }}>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
           <InfoCard />
         </Grid>
       </Grid>
