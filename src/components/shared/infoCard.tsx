5de410b8d94fb99cc25726507e3ce95c5eb364c4
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-08 21:20:00 +0100

    update

--- src/components/shared/infoCard.tsx
+++ src/components/shared/infoCard.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import styled from '@emotion/styled';
+import Image from 'next/image';
 import { Box, IconButton, Theme, Typography, useTheme } from '@mui/material';
 //import DiscountIcon from '@mui/icons-material/Discount';
 //import PercentIcon from '@mui/icons-material/Percent';
 import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
-import LanguageTwoToneIcon from '@mui/icons-material/LanguageTwoTone';
+//import LanguageTwoToneIcon from '@mui/icons-material/LanguageTwoTone';
 const Wrap = styled(Box)<{ theme?: Theme }>`
   width: 300px;
   max-width: 100%;
@@ -15,18 +16,20 @@ const Wrap = styled(Box)<{ theme?: Theme }>`
   flex-direction: column;
   text-align: center;
 `;
+interface Props {
+  icon: ReactNode;
+  title: string;
+  content: string;
+}
 
-function InfoCard() {
+function InfoCard({ icon, title, content }: Props) {
   const theme = useTheme();
   return (
     <Wrap>
-      <IconButton>
-        <ShoppingCartTwoToneIcon sx={{ fontSize: '30px' }} />
-      </IconButton>
-      <Typography variant="h6">APPPPP</Typography>
+      <Image src="/world.png" width="30" height="30" />
+      <Typography variant="h6">{title}</Typography>
       <Typography sx={{ marginTop: 1, color: theme.colors.secondary.light }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-        totam dicta quisquam. Laborum tenetur repellendus culpa iusto.
+        {content}
       </Typography>
     </Wrap>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Container, Grid } from '@mui/material';
 import Welcome from '../components/landingPage/welcome';
-import GridImage from '../components/main/gridImage';
+import GridImage from '../components/landingPage/gridImage';
 import InfoCard from '../components/shared/infoCard';
 import Main from '../layout/main';
-
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
+//import LanguageTwoToneIcon from '@mui/icons-material/LanguageTwoTone';
 const images = ['/jacket.jpg', 'suit.jpg', 'womansuit.jpg', 'mansuit.jpg'];
 
 const Home = () => (
@@ -12,7 +13,7 @@ const Home = () => (
     <Container sx={{ paddingY: 4 }}>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
         <Grid item xs={12} sm={6} md={3}>
-          <InfoCard />
+          <InfoCard  />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
           <InfoCard />
