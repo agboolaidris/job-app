f9384e465a337e2e8af4e3700a1f3acb23149016
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-08 21:05:30 +0100

    update

--- src/components/shared/infoCard.tsx
+++ src/components/shared/infoCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Box, Theme, Typography, useTheme } from '@mui/material';
+import { Box, IconButton, Theme, Typography, useTheme } from '@mui/material';
+//import DiscountIcon from '@mui/icons-material/Discount';
+//import PercentIcon from '@mui/icons-material/Percent';
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
+import LanguageTwoToneIcon from '@mui/icons-material/LanguageTwoTone';
 const Wrap = styled(Box)<{ theme?: Theme }>`
   width: 300px;
   max-width: 100%;
-  height: 300px;
+  min-height: 300px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,8 +20,11 @@ function InfoCard() {
   const theme = useTheme();
   return (
     <Wrap>
-      <Typography variant="h5">APPPPP</Typography>
-      <Typography sx={{ marginTop: 2, color: theme.colors.secondary.light }}>
+      <IconButton>
+        <ShoppingCartTwoToneIcon sx={{ fontSize: '30px' }} />
+      </IconButton>
+      <Typography variant="h6">APPPPP</Typography>
+      <Typography sx={{ marginTop: 1, color: theme.colors.secondary.light }}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
         totam dicta quisquam. Laborum tenetur repellendus culpa iusto.
       </Typography>
