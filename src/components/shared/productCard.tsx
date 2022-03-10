ae5bb352aebd8b8654f9096a4453c12959eb0207
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-10 09:51:38 +0100

    update

--- src/components/shared/productCard.tsx
+++ src/components/shared/productCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import { Box, Typography } from '@mui/material';
 const CardStyled = styled.div`
   width: 300px;
   max-width: 100%;
@@ -15,6 +16,12 @@ function ProductCard() {
       <div className="imgBox">
         <Image src="/jacket.jpg" width={300} height={300} alt="jacket" />
       </div>
+      <Box>
+        <Typography sx={{ fontWeight: 'bolder' }}>
+          Lorem ipsum, dolor sit amet
+        </Typography>
+        <Typography>$70</Typography>
+      </Box>
     </CardStyled>
   );
 }
