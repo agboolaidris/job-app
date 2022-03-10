831e013f6aa903ee380fb6ca8e28c8405e3dd9af
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-10 09:25:08 +0100

    update

--- src/components/shared/productCard.tsx
+++ src/components/shared/productCard.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-c
+import styled from '@emotion/styled';
+import Image from 'next/image';
+const CardStyled = styled.div`
+  width: 300px;
+  max-width: 100%;
+  .imgBox {
+    width: 100%;
+    height: 300px;
+  }
+`;
 function ProductCard() {
-  return <div>P</div>;
+  return (
+    <CardStyled>
+      <div className="imgBox">
+        <Image src="/jacket.png" layout="fill" alt="jacket" />
+      </div>
+    </CardStyled>
+  );
 }
 
 export default ProductCard;
