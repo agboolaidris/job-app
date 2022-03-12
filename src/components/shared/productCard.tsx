274032314021839dc9392793526d5cf5a4e582dc
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-12 07:09:05 +0100

    update

--- src/components/shared/productCard.tsx
+++ src/components/shared/productCard.tsx
@@ -21,6 +21,17 @@ const CardStyled = styled.div<{ theme?: Theme }>`
       width: 100%;
       min-height: 100px;
       transform: translateY(100%);
+      padding: 10px;
+      .sizeBox {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .size {
+          width: 40px;
+          height: 50px;
+          border: 1px solid red;
+        }
+      }
     }
     &:hover {
       .cart-content {
@@ -34,7 +45,13 @@ function ProductCard() {
     <CardStyled>
       <div className="imgBox">
         <Image src="/jacket.jpg" width={300} height={400} alt="jacket" />
-        <div className="cart-content">hhhh</div>
+        <div className="cart-content">
+          <div className="sizeBox">
+            <div className="size">SX</div>
+            <div className="size">SX</div> <div className="size">SX</div>
+            <div className="size">SX</div>
+          </div>
+        </div>
       </div>
 
       <Box>
