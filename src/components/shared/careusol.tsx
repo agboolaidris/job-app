cd61f3f397571af0f74569a1fdccaf99e519f90a
idris agboola <agboolaisholaidreez@gmail.com>
2022-03-17 16:41:19 +0100

    update

--- src/components/shared/careusol.tsx
+++ src/components/shared/careusol.tsx
@@ -1,25 +1,32 @@
 import React, { ReactNode, useState } from 'react';
-import { Box, Stack, Theme } from '@mui/material';
+import { Box, Stack, Theme, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 import Button from './button';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 interface Props {
   cards: ReactNode[];
 }
 
-const Wrap = styled(Box)<{ theme?: Theme }>`
+const Wrap = styled.div<{ theme?: Theme }>`
   padding: 100px 0;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   overflow: hidden;
   padding: 30px;
+  width: 100%;
   ${({ theme }) => theme.breakpoints.down('md')} {
-    flex-direction: column-reverse;
+    flex-direction: column;
   }
-  .button {
-    width: max-content;
-    display: flex;
-    padding-right: 10px;
+  .side {
+    padding: 0px 10px;
+    width: 100%;
+    .button {
+      padding: 80px 0;
+      display: flex;
+      justify-content: center;
+    }
   }
 `;
 
@@ -41,9 +48,18 @@ function Careusol({ cards }: Props) {
 
   return (
     <Wrap>
-      <div className="button">
-        <Button onClick={handlePrevious}>Previous</Button>
-        <Button onClick={handleNext}>Next</Button>
+      <div className="side">
+        <Typography variant="h4" sx={{ width: 'max-content' }}>
+          Our Top Pick
+        </Typography>
+        <div className="button">
+          <Button onClick={handlePrevious}>
+            <ArrowBackIosNewIcon />
+          </Button>
+          <Button onClick={handleNext}>
+            <ArrowForwardIosIcon />
+          </Button>
+        </div>
       </div>
       <Stack direction="row" spacing={2}>
         {items.map((card) => card)}
