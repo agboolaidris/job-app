42f3f7ff5e30d0f34e42912de27a39b6f9067737
Iris <agboolaisholaidreez>
2023-07-10 00:36:49 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['tailwindui.com', 'images.unsplash.com'],
+    domains: ['images.unsplash.com'],
   },
   reactStrictMode: true,
-  webpack5: true,
 };
 
 module.exports = nextConfig;
